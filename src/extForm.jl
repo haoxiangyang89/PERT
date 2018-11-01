aba486b3c1bd3795c77e02d9ca2ca6afcc979a19
Haoxiang Yang <haoxiangyang89@gmail.com>
2018-11-01 12:58:07 -0500

    Change the solver for the extensive form

--- src/extForm.jl
+++ src/extForm.jl
@@ -25,8 +25,8 @@ function extForm(pData,disData,Ω,TL = Inf)
         M[ω] = 300;
     end
 
-    mp = Model(solver = GurobiSolver(IntFeasTol = 1e-9, TimeLimit = TL));
-    # mp = Model(solver = CbcSolver());
+    # mp = Model(solver = GurobiSolver(IntFeasTol = 1e-9, TimeLimit = TL));
+    mp = Model(solver = CplexSolver(CPX_PARAM_EPRHS = 1e-8,CPX_PARAM_EPINT = 1e-8,CPX_PARAM_TILIM = TL));
     @variable(mp,t0[i in pData.II] >= 0);
     @variable(mp,0 <= x0[i in pData.II, j in pData.Ji[i]] <= 1);
     @variable(mp,t[i in pData.II, ω in Ω] >= 0);
@@ -70,8 +70,8 @@ function extForm_cheat(pData,disData,Ω,prec = 1e-4,TL = Inf)
         M[ω] = sum(max(pData.D[i],pData.D[i]+disData[ω].d[i]) for i in pData.II if i != 0);
     end
 
-    mp = Model(solver = GurobiSolver(IntFeasTol = 1e-9, TimeLimit = TL,MIPGap = prec));
-    # mp = Model(solver = CbcSolver());
+    #mp = Model(solver = GurobiSolver(IntFeasTol = 1e-9, TimeLimit = TL,MIPGap = prec));
+    mp = Model(solver = CplexSolver(CPX_PARAM_EPRHS = 1e-8,CPX_PARAM_EPINT = 1e-8,CPX_PARAM_TILIM = TL));
     @variable(mp,t0[i in pData.II] >= 0);
     @variable(mp,0 <= x0[i in pData.II, j in pData.Ji[i]] <= 1);
     @variable(mp,t[i in pData.II, ω in Ω] >= 0);
