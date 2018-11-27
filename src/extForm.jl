b9f055e079d55783d7ef97ee9ccde32dd592c152
Haoxiang Yang <haoxiangyang89@gmail.com>
2018-11-27 10:29:56 -0600

    Fixed the extForm error

--- src/extForm.jl
+++ src/extForm.jl
@@ -115,7 +115,7 @@ function extForm_cheat(pData,disData,Ω,prec = 1e-4,TL = Inf)
     @constraint(mp, durationConstr2[k in pData.K], t0[k[2]] - t0[k[1]] >= pData.D[k[1]]*(1 - sum(pData.eff[k[1]][j]*x0[k[1],j] for j in pData.Ji[k[1]])));
     @constraint(mp, xConstr[i in pData.II,ω in Ω], sum(x[i,j,ω] for j in pData.Ji[i]) <= 1);
     @constraint(mp, xConstr0[i in pData.II], sum(x0[i,j] for j in pData.Ji[i]) <= 1);
-    @constraint(mp, GGcons1[i in pData.II, ω in 1:(length(HList) - 1)], G[i,HList[ω]] >= G[i,HList[ω] + 1]);
+    @constraint(mp, GGcons1[i in pData.II, ω in 1:(length(HList) - 1)], G[i,HList[ω]] >= G[i,HList[ω + 1]]);
     @constraint(mp, GGcons2[k in pData.K, ω in HList], G[k[2],ω] >= G[k[1],ω]);
     @constraint(mp, budgetConstr[ω in Ω], sum(sum(pData.b[i][j]*x[i,j,ω] for j in pData.Ji[i]) for i in pData.II) <= pData.B);
     @constraint(mp, budgetConstr0, sum(sum(pData.b[i][j]*x0[i,j] for j in pData.Ji[i]) for i in pData.II) <= pData.B);
